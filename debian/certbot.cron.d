d6cd32ec011d106ec8174edfe5bbc3ef12e6e595
Harlan Lieberman-Berg <hlieberman@setec.io>
2016-06-02 20:03:31 -0400

    Fix cron file to run only twice. (Closes: #825732)

--- debian/certbot.cron.d
+++ debian/certbot.cron.d
@@ -3,4 +3,4 @@
 # Eventually, this will be an opportunity to validate certificates
 # haven't been revoked, etc.  Renewal will only occur if expiration
 # is within 30 days.
-* */12 * * * root perl -e 'sleep int(rand(3600))'; certbot -q renew
+0 */12 * * * root perl -e 'sleep int(rand(3600))'; certbot -q renew
