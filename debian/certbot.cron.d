68397bc9574f25937b1de36311d65527cda7ac14
Harlan Lieberman-Berg <hlieberman@setec.io>
2016-07-07 21:08:41 -0400

    Recraft crontab to ensure PATH is correct
    certbot attempts to find webserver daemons in $PATH.  Because cron
    PATH doesn't include /usr/sbin by default, this can cause errors when
    using certain certbot plugins. Additionally, per policy, we must test
    for the existance of the certbot executable in case someone has
    removed but not purged the package.

--- debian/certbot.cron.d
+++ debian/certbot.cron.d
@@ -1,6 +1,11 @@
+# /etc/cron.d/certbot: crontab entries for the certbot package
+#
 # Upstream recommends attempting renewal twice a day
 #
 # Eventually, this will be an opportunity to validate certificates
 # haven't been revoked, etc.  Renewal will only occur if expiration
 # is within 30 days.
-0 */12 * * * root perl -e 'sleep int(rand(3600))'; certbot -q renew
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+0 */12 * * * root test -x /usr/bin/certbot && perl -e 'sleep int(rand(3600))' && certbot -q renew
